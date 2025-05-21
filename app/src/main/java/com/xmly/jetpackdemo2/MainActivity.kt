e994bd4d719e37c118c478a4e08324e3d12cb7c0
mingchuan.yang <mingchuan.yang@ximalaya.com>
2025-05-21 16:58:32 +0800

    [feat] 使用 MaterialTheme

--- app/src/main/java/com/xmly/jetpackdemo2/MainActivity.kt
+++ app/src/main/java/com/xmly/jetpackdemo2/MainActivity.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.MarqueeSpacing
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,9 +23,12 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.xmly.jetpackdemo2.ui.theme.JetpackDemo2Theme
 
@@ -24,7 +36,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackDemo2Theme {
+            MaterialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,29 +52,48 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SimpleWidgetColumn() {
     val context = LocalContext.current
-    Column {
-        Text(
-            text = "This is Text",
-            color = Color.Red,
-            fontSize = 24.sp
-        )
-        Button(onClick = {
-            Toast.makeText(context, "This is Toast", Toast.LENGTH_SHORT).show()
-        }) {
-            Text(
-                text = "This is Button",
-                color = Color.White,
-                fontSize = 26.sp
+    Surface(
+        shape = MaterialTheme.shapes.medium, // 使用 MaterialTheme 自带的形状
+        modifier = Modifier.padding(all = 8.dp)
+    ) {
+        Column {
+            Row (modifier = Modifier.padding(all = 8.dp)){
+
+                Image(
+                    painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "profile picture", //这个描述用于无障碍
+                    modifier = Modifier
+                        .size(50.dp) // 改变 Image 元素的大小
+                        .clip(CircleShape) // 将图片裁剪成圆形
+                        .border(border = BorderStroke(1.dp, color = Color.Blue), shape = CircleShape) // 添加边框
+                )
+                Spacer(modifier = Modifier.padding(horizontal = 8.dp))
+                Text(
+                    text = "This is Text",
+                    color = MaterialTheme.colorScheme.error, // 添加颜色
+                    fontSize = 24.sp
+                )
+            }
+
+            Button(onClick = {
+                Toast.makeText(context, "This is Toast", Toast.LENGTH_SHORT).show()
+            }) {
+                Text(
+                    text = "This is Button",
+                    color = Color.White,
+                    fontSize = 26.sp
+                )
+            }
+            TextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Type something here")
+                }
             )
         }
-        TextField(
-            value = "",
-            onValueChange = {},
-            placeholder = {
-                Text(text = "Type something here")
-            }
-        )
     }
+
 }
 
 @Composable
@@ -76,7 +107,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackDemo2Theme {
-        Greeting("Android")
+    MaterialTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            SimpleWidgetColumn()
+        }
     }
 }
